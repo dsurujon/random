9127b44fa4b2b29cf9d8b1d90b3b21da5c23cf0d
Defne <defnesurujon@gmail.com>
2018-02-20 13:05:17 -0500

    Create make_prot_table.py

---
+++ make_prot_table.py
@@ -0,0 +1,54 @@
+## Defne Surujon
+## Feb 20, 2018
+
+# make a prot_table file from a genbank
+# 9 columns without headers: gene annotation, start, end,
+# strand, protein lenght, nothing, nothing, nothing, locus_tag
+
+from Bio import SeqIO
+from optparse import OptionParser
+
+
+options = OptionParser(usage='%prog input output',
+                       description="Specify input directory and output file")
+
+options.add_option("-i","--infile",dest="inputfile",
+                   help="Input file (.gbk)")
+options.add_option("-o","--outfile",dest="outputfile",
+                   help="output file (.prot_table)")
+
+
+def main():
+    opts, args = options.parse_args()
+    infile = opts.inputfile
+    outfile = opts.outputfile
+    
+    mystrain=SeqIO.read(infile,"genbank")
+    prot_table = []
+    stranddict = {1:'+',-1:'-'}
+
+    for thisfeature in mystrain.features:
+        if thisfeature.type=="CDS":
+            try:
+                thisline = ["" for i in range(0,9)]
+                thisline[0] = thisfeature.qualifiers['product'][0]
+                thisline[1] = str(thisfeature.location.start)
+                thisline[2] = str(thisfeature.location.end)
+                thisline[3] = stranddict[thisfeature.location.strand]
+                thisline[4] = int(abs(thisfeature.location.end-thisfeature.location.start)/3)
+                thisline[8] = thisfeature.qualifiers['locus_tag'][0]
+                
+                prot_table.append(thisline)
+            except KeyError: pass
+
+    g = open(outfile,'w')
+    for line in prot_table:
+        linestr = [str(i) for i in line]
+        g.write("\t".join(linestr)+"\n")
+    g.close()
+
+
+
+if __name__ == '__main__':
+    main()
+		
